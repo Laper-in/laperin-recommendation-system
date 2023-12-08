7b88650df91885709b0defba0aaa846f490d7b56
Haris Hartanto <muhamadharishartanto@gmail.com>
2023-12-08 22:35:11 +0700

    add comments & set cos_sim score treshold

--- recommender.py
+++ recommender.py
@@ -9,25 +9,32 @@ class Recommender:
         self.db_acc = DatabaseAccess()
         self.txt_prep = TextPreprocessing()
         self.table = 'recipe_ingredients'
+        self.cos_sim_threshold = 0.4
 
     def recommend_recipes(self, user_ingredients):
+        # read recipe ids and ingredients from database
         recipe_ids = self.db_acc.read_id(self.table)
         ingredients = self.db_acc.read_ingredients(self.table)
-
+        
+        # convert sql query result to list 1d (list of recipe ids and list of ingredients)
         recipe_ids_ls = self.txt_prep.sql_query_to_list(recipe_ids)
         ingredients_ls = self.txt_prep.sql_query_to_list(ingredients)
-
+        
+        # combine user ingredients and recipe ingredients
         docs = self.txt_prep.combine_query_content(user_ingredients, ingredients_ls)
-
+        
+        # calculate tf-idf and cosine similarity between user ingredients and recipe ingredients
         vectorizer = TfidfVectorizer(smooth_idf=False, norm=None)
         docs_weight = vectorizer.fit_transform(docs)
         query_weight = vectorizer.transform([user_ingredients])
         cos_sim = cosine_similarity(query_weight, docs_weight).flatten()
-
+        
+        # adjust cosine similarity score and recipe ids
         cos_sim = cos_sim[1:]
         cos_sim_dict = dict(enumerate(cos_sim))
-        cos_sim_dict = {k: v for k, v in sorted(cos_sim_dict.items(), key=lambda item: item[1], reverse=True) if v > 0.4}
+        cos_sim_dict = {k: v for k, v in sorted(cos_sim_dict.items(), key=lambda item: item[1], reverse=True) if v > self.cos_sim_threshold}
 
+        # set corresponding recipe ids based on cosine similarity score
         idx_for_id = list(cos_sim_dict.keys())
         recipe_ids_rank = [recipe_ids_ls[i] for i in idx_for_id]
 
