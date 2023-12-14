e46060fd285c7c979e49abe4af7afaf1c8541005
Haris Hartanto <muhamadharishartanto@gmail.com>
2023-12-14 20:52:51 +0700

    update sql query functions based on new table structure

--- db_access.py
+++ db_access.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import hashlib
 
 class Database:
     def __init__(self):
@@ -21,7 +22,7 @@ class DatabaseAccess:
     def read_id(self, table_name, mode='ASC'):
         '''Read recipe ids from database'''
         try:
-            sql = 'SELECT recipe_id FROM {} ORDER BY recipe_id {}'.format(table_name, mode)
+            sql = 'SELECT idRecipe FROM {} ORDER BY idRecipe {}'.format(table_name, mode)
             self.database.my_cursor.execute(sql)
             result = self.database.my_cursor.fetchall()
             return result
@@ -31,28 +32,28 @@ class DatabaseAccess:
     def check_not_exist_id(self, first_table, second_table):
         '''Check recipe ids that not exist in second table'''
         try:
-            sql = 'SELECT a.recipe_id FROM {} a WHERE a.recipe_id NOT IN (SELECT DISTINCT p.recipe_id FROM {} p) ORDER BY recipe_id'.format(first_table, second_table)
+            sql = 'SELECT a.id FROM {} a WHERE a.id NOT IN (SELECT DISTINCT p.idRecipe FROM {} p) ORDER BY id'.format(first_table, second_table)
             self.database.my_cursor.execute(sql)
             result = self.database.my_cursor.fetchall()
             return result
         except mysql.connector.Error as e:
             return str(e)
 
-    def read_ingredients(self, table_name, mode='ASC'):
+    def read_ingredients(self, table_name, id_column, mode='ASC'):
         '''Read ingredients from database'''
         try:
-            sql = 'SELECT ingredient FROM {} ORDER BY recipe_id {}'.format(table_name, mode)
+            sql = 'SELECT ingredient FROM {} ORDER BY {} {}'.format(table_name, id_column, mode)
             self.database.my_cursor.execute(sql)
             result = self.database.my_cursor.fetchall()
             return result
         except mysql.connector.Error as e:
             return str(e)
         
-    def read_ingredients_by_ids(self, table_name, recipe_ids):
+    def read_ingredients_by_ids(self, table_name, id_column, recipe_ids):
         '''Read ingredients from database by recipe ids'''
     
         try:
-            sql = 'SELECT ingredient FROM {} WHERE recipe_id IN ({})'.format(table_name, recipe_ids)
+            sql = 'SELECT ingredient FROM {} WHERE {} IN ({})'.format(table_name, id_column, recipe_ids)
             self.database.my_cursor.execute(sql)
             result = self.database.my_cursor.fetchall()
             return result
@@ -62,7 +63,10 @@ class DatabaseAccess:
     def insert_many_ingredients(self, list_data, table_name='recipe_details'):
         '''Insert multiple ingredients to database'''
         try:
-            sql = 'INSERT INTO {} (recipe_id, ingredient) VALUES (%s, %s)'.format(table_name)
+            sql = 'INSERT INTO {} (id, idRecipe, ingredient) VALUES (%s, %s, %s)'.format(table_name)
+            # modified list_data by adding new id column that using random string
+            list_data = [(hashlib.md5((str(item[0]) + item[1]).encode()).hexdigest()[:10], item[0], item[1]) for item in list_data]
+
             self.database.my_cursor.executemany(sql, list_data)
             self.database.my_db.commit()
             return self.database.my_cursor.rowcount
